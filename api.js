e581eef4d9174b23625236c4ddc7c861936688f6
fireconta <fireconta02@gmail.com>
2025-06-14 19:14:45 -0300

    Update api.js

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const OneSignal = require('onesignal-node');
 require('dotenv').config();
 
 const client = new OneSignal.Client(process.env.ONESIGNAL_APP_ID, process.env.ONESIGNAL_API_KEY);
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
+const mongoClient = new MongoClient('mongodb+srv://loganccs_admin:V8JCm5teHVQEGFxw@loganccs-cluster.hlc6mov.mongodb.net/loganccs?retryWrites=true&w=majority');
 
 let db;
 let isInitialized = false;
